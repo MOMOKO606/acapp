6ce95dd712095a541b41d1aa967b8d0f1aa2cc5e
Long Bian <0618bianlong@gmail.com>
2023-12-22 11:21:35 +0800

    Add a menu page.

--- game/views.py
+++ game/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    line1 = '<h1 style="text-align: center">DOTA</h1>'
+    line1 = '<h1 style="text-align: center">Welcome to the DOTA world</h1>'
     line2 = '<p style="text-align: center"><img src="https://dmarket.com/blog/best-dota2-wallpapers/dota2logo_hu63a418b03843b39b133100730abc88ee_104613_1920x1080_resize_q100_lanczos.jpg" width=1200></p>'
     return HttpResponse(line1 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">Game Menu</h1>'
+    line2 = '<p style="text-align: center"><img src="https://dmarket.com/blog/best-dota2-wallpapers/dota2heroes9_hu9c86aa7e6a6fc7a27835e96ba206b0fc_211614_1920x1080_resize_q100_lanczos.jpg" width=1200></p>'
+    return HttpResponse(line1 + line2)
+
+
