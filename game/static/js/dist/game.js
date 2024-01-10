af3d57531200f99df2633aa761036b22a8da1978
Long Bian <0618bianlong@gmail.com>
2024-01-11 07:05:24 +0800

    update the interactions between login and register windows

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -480,84 +480,98 @@ class Settings{
         this.photo = "";
 
         this.$settings = $(`
-            <div class="ac-game-settings">
-                <div class="ac-game-settings-login">
-                    <div class="ac-game-settings-title">
-                        Login
-                    </div>
-                    <div class="ac-game-settings-username">
-                        <div class="ac-game-settings-item">
-                            <input type="text" placeholder="Username">
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-password">
-                        <div class="ac-game-settings-item">
-                            <input type="password" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-submit">
-                        <div class="ac-game-settings-item">
-                            <button>Login to your account</button>
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-error-messages">
-                            Invalid username or password
-                    </div>
-                    <div class="ac-game-settings-option">
-                            Register
-                    </div>
-                    <div class="ac-game-settings-google">
-                        <button class="gsi-material-button">
-                          <div class="gsi-material-button-state"></div>
-                            <div class="gsi-material-button-content-wrapper">
-                                <div class="gsi-material-button-icon">
-                                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
-                                              <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-                                                      <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
-                    <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-                    <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-                    <path fill="none" d="M0 0h48v48H0z"></path>
-                  </svg>
+<div class="ac-game-settings">
+    <div class="ac-game-settings-login">
+        <div class="ac-game-settings-title">
+            Login
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="Username">
+            </div>
+        </div>
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>Login to your account</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-message">
+            Invalid username or password
+        </div>
+        <div class="ac-game-settings-option">
+            Register
+        </div>
+        <div class="ac-game-settings-google">
+            <button class="gsi-material-button">
+            <div class="gsi-material-button-state"></div>
+            <div class="gsi-material-button-content-wrapper">
+                <div class="gsi-material-button-icon">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+                        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                        <path fill="none" d="M0 0h48v48H0z"></path>
+                    </svg>
                 </div>
                 <span class="gsi-material-button-contents">Continue with Google</span>
                 <span style="display: none;">Continue with Google</span>
-              </div>
-            </button>
-                    </div>
-                </div>
-            <div class="ac-game-settings-register">
-                    <div class="ac-game-settings-title">
-                       Register
-                    </div>
-                    <div class="ac-game-settings-username">
-                        <div class="ac-game-settings-item">
-                            <input type="text" placeholder="Username">
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-password">
-                        <div class="ac-game-settings-item">
-                            <input type="password" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-password">
-                        <div class="ac-game-settings-item">
-                            <input type="password" placeholder="Confirm Password">
-                        </div>
-                    <div class="ac-game-settings-submit">
-                        <div class="ac-game-settings-item">
-                            <button>Sign up</button>
-                        </div>
-                    </div>
-                    <div class="ac-game-settings-option">
-                            Login
-                    </div>
             </div>
+            </button>
+        </div>
+    </div>
+
+    <div class="ac-game-settings-register">
+        <div class="ac-game-settings-title">
+            Register
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="Username">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-first">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Password">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-second">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="Confirm Password">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>Sign up</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-option">
+            Go Back to Login
+        </div>
+    </div>
+</div>   
         `);
         this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit button");
+        this.$login_error_message = this.$login.find(".ac-game-settings-error-message");
+        this.$login_register = this.$login.find(".ac-game-settings-option");
 
         this.$login.hide();
 
         this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".ac-game-settings-error-message");
+        this.$register_login = this.$register.find(".ac-game-settings-option");
 
         this.$register.hide();
 
@@ -568,7 +582,30 @@ class Settings{
 
     start(){
         this.getinfo();
+        this.add_listening_events();
     }
+
+
+    add_listening_events(){
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+
+    add_listening_events_login(){
+        let outer = this;
+        this.$login_register.click(function(){
+            outer.register();
+        });
+    }
+
+    add_listening_events_register(){
+        let outer = this;
+        this.$register_login.click(function(){
+            outer.login();
+        });
+    }
+
 
     //  打开注册界面
     register(){
@@ -598,8 +635,8 @@ class Settings{
                     outer.hide();
                     outer.root.menu.show();
                 }else{
-                    // outer.login();
-                    outer.register();
+                    outer.login();
+                    // outer.register();
                 }
             }
         });
