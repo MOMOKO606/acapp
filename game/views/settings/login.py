754a1f7da4fb815d4bb2ee88e52c805f66814582
Long Bian <0618bianlong@gmail.com>
2024-01-11 10:10:24 +0800

    add login controller and its url filr

---
+++ game/views/settings/login.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+
+def signin(request):
+    data = request.GET
+    username = data.get("username")
+    password = data.get("password")
+    #  验证函数
+    user = authenticate(username = username, password = password)
+    if not user:
+        return JsonResponse({
+            "result": "Invalid Username or Password"
+        })
+    #  登录    
+    login(request, user)
+    return JsonResponse({
+        "result": "success"
+    })
