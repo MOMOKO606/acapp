88b079b1c4ac907b251d4109052635e3f3306b80
Long Bian <0618bianlong@gmail.com>
2024-01-24 11:37:53 +0800

    update the sizes of particle & fireball to be relative

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -155,7 +155,7 @@ class Particle extends AcGameObject{
         this.speed = speed;
         this.move_length = move_length;
         this.friction = 0.9;
-        this.eps = 0.1;
+        this.eps = 0.01;
     }
 
     start(){
@@ -175,8 +175,9 @@ class Particle extends AcGameObject{
     }
 
     render(){
+        let scale = this.playground.scale;
         this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.arc(this.x * scale, this.y * scale, this.radius * scale, 0, Math.PI * 2, false);
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
     }
@@ -388,7 +389,7 @@ class FireBall extends AcGameObject{
         this.speed = speed;
         this.move_length = move_length;
         this.damage = damage;
-        this.eps = 0.1;
+        this.eps = 0.01;
     }
 
     start(){
@@ -435,8 +436,9 @@ class FireBall extends AcGameObject{
 
 
     render(){
+        let scale = this.playground.scale;
         this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.arc(this.x * scale, this.y * scale, this.radius * scale, 0, Math.PI * 2, false);
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
     }
