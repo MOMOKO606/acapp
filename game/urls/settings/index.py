fdda803913026147216b26d11c4b46ac4934ac48
Long Bian <0618bianlong@gmail.com>
2024-02-14 05:53:04 +0800

    added django rest framework jwt

--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -3,8 +3,14 @@ from game.views.settings.getinfo import getinfo
 from game.views.settings.login import signin
 from game.views.settings.logout import signout
 from game.views.settings.register import register
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name='settings_token'),
+    path("token/refresh", TokenRefreshView.as_view(), name='settings_token_refresh'),
     path("getinfo/", getinfo, name = "settings_getinfo"),
     path("login/", signin, name = "settings_login"),
     path("logout/", signout, name = "settings_logout"),
